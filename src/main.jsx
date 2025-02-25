d3239e1522b0fcd30dab4fbdcf7b8ccffb28cd7e
mbonikev <mbonimpayekevin@gmail.com>
2025-02-25 08:33:44 +0200

    updated src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)

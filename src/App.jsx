87c5adc9f3ab62575a60c7a63fa680b581f7bb49
mbonikev <mbonimpayekevin@gmail.com>
2025-02-25 08:38:14 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Login />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 

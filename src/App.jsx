82de0ce8e2fc9070053fa810cb83c2e827e2a6f5
mbonikev <mbonimpayekevin@gmail.com>
2025-02-25 08:20:44 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <div>App</div>;
-}
+  return <div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} /> {/* For any invalid route */}
+      </Routes>
+  </div>;
+} 
 
 export default App;

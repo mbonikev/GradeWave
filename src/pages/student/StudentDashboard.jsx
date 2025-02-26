56ceb5e64aced1f21e81268210100ed5ec6591d2
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 08:38:14 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -24,7 +24,7 @@ function StudentDashboard() {
   useEffect(() => {
     setTimeout(() => {
       setFetching(false);
-    }, 3500);
+    }, 1500);
   }, []);
   return (
     <div className="w-full min-h-svh overflow-y-auto flex-1 flex">

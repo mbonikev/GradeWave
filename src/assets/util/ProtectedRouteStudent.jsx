04657c4964c94ffc83a9595b68ba68ee1aa2102f
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 09:55:47 +0200

    updated src/assets/util/ProtectedRouteStudent.jsx

--- src/assets/util/ProtectedRouteStudent.jsx
+++ src/assets/util/ProtectedRouteStudent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import {useNavigate } from 'react-router-dom';
+
 
 function ProtectedRouteStudent({ children }) {
   const loggedIn =

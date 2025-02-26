1c64a23a1f5aa7a7ac863b50b0707bf73544c774
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 08:36:44 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -43,9 +43,7 @@ function StudentDashboard() {
           ></div>
         )}
         <StudentNavbar openSidebar={openSidebar} />
-        <div className="w-full flex-1 overflow-y-auto py-1 flex flex-col gap-0 pt-5 relative">
-          {fetching ? <LoadingScreen /> : <></>}
-        </div>
+        <div className="w-full flex-1 overflow-y-auto py-1 flex flex-col gap-0 pt-5 relative"></div>
       </div>
     </div>
   );

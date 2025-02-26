b879ad67eaf8b1a503d1f5616370208f9224590c
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 08:54:15 +0200

    updated src/components/LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -4,8 +4,8 @@ import { IntroGif } from "../assets";
 function LoadingScreen({ animateFetching }) {
   return (
     <div
-      className={`w-full h-svh fixed top-0 left-0 z-50 bg-white/90 flex items-center justify-center transition-all ease-in-out duration-300 ${
-        animateFetching ? "opacity-100" : "opacity-0"
+      className={`w-full h-svh fixed top-0 left-0 z-50 bg-white flex items-center justify-center ${
+        animateFetching ? "opacity-100" : "opacity-0 transition-all ease-out duration-500"
       }`}
     >
       <video

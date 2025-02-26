210dfd2d02fcb8fb09b911c8aa268b4de1c0f8e3
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 10:23:18 +0200

    updated src/components/student/StudentNavbar.jsx

--- src/components/student/StudentNavbar.jsx
+++ src/components/student/StudentNavbar.jsx
@@ -36,12 +36,12 @@ function StudentNavbar({ title, openSidebar }) {
         <img src={LogoBlue} className="hidden max-sm:flex h-8 ml-2" />
       </div>
       <div className="flex items-center justify-end gap-1">
-        <button className="text-lightBlackText/80 hover:text-mainColor p-1 h-[40px] aspect-square flex items-center justify-center max-sm:bg-stone-100 hover:bg-stone-100 rounded-xl">
+        {/* <button className="text-lightBlackText/80 hover:text-mainColor p-1 h-[40px] aspect-square flex items-center justify-center max-sm:bg-stone-100 hover:bg-stone-100 rounded-xl">
           <LuSearch className="text-xl" />
         </button>
         <button className="text-lightBlackText/80 hover:text-mainColor p-1 h-[40px] aspect-square flex items-center justify-center max-sm:bg-stone-100 hover:bg-stone-100 rounded-xl">
           <IoCardOutline className="text-2xl" />
-        </button>
+        </button> */}
         <button
           onClick={handleLoginStudent}
           className="text-lightBlackText/80 hover:text-mainColor p-1 h-[40px] aspect-square flex items-center justify-center max-sm:bg-stone-100 hover:bg-stone-100 rounded-xl"

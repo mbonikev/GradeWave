5d7ddf587c227d7b6f5ecfc535244c84b070ebfd
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 08:49:44 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
@@ -14,6 +14,8 @@ import StudentSettings from "./pages/student/StudentSettings";
 import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
+  const [fetching, setFetching] = useState(true);
+
   useEffect(() => {
     setTimeout(() => {
       setFetching(false);

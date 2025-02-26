38624a2a72d9cc53e6b02e4c271f0594aaa28c8f
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 09:48:47 +0200

    updated src/assets/util/ProtectedRouteStudent.jsx

--- src/assets/util/ProtectedRouteStudent.jsx
+++ src/assets/util/ProtectedRouteStudent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 function ProtectedRouteStudent({ children }) {
-  const loggedIn = sessionStorage.getItem('loggedIn') === 'true';
+  const loggedIn = sessionStorage.getItem('loggedInStudent') === 'false' || null;
 
   if (!loggedIn) {
     return <Redirect to="/" />;

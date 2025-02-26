5817a3587d3cf8c2508cbd7f1338d0d32bac23ca
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 09:16:45 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -7,7 +7,7 @@ import LoadingScreen from "../../components/LoadingScreen";
 function StudentDashboard() {
   const [showSidebar, setShowSidebar] = useState(false);
   const [animateShowSidebar, setAnimateShowSidebar] = useState(false);
-  const [fetching, setFetching] = useState(false);
+  const [fetching, setFetching] = useState(true);
   const [animateFetching, setAnimateFetching] = useState(true);
 
   const openSidebar = () => {

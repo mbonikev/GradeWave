199dec3b3ec8465ac2969047aef388a0da33e0f3
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 09:05:15 +0200

    updated src/pages/NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div className='w-full min-h-svh bg-red-300'>NotFound</div>
+    <div className='w-full min-h-svh bg-red-300 z-50 relative'>NotFound</div>
   )
 }
 

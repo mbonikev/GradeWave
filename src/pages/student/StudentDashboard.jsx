841a37cb98c575b7abe2372c05756b65535258b0
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 17:37:52 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -37,35 +37,31 @@ function StudentDashboard() {
             }`}
           ></div>
         )}
-        <div className="sticky top-0 ">
-          <StudentNavbar openSidebar={openSidebar} />
-        </div>
-        <div className="w-full flex-1 flex flex-col gap-0 py-5 px-3 max-lg:px-5 relative">
-          {/* content */}
-          <div className="w-full h-fit max-w-[1100px] min-h-[2000px] mx-auto">
-            <h1 className="text-[28px] font-semibold">Dashboard</h1>
+        <StudentNavbar openSidebar={openSidebar} />
+        {/* content */}
+        <div className="w-full h-fit max-w-[1100px] min-h-[1000px] mx-auto">
+          <h1 className="text-[28px] font-semibold">Dashboard</h1>
 
-            <div className="w-full h-fit grid grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4 mt-3 py-1">
-              {[
-                { name: "Check Results", path: "/student_dashboard" },
-                { name: "Register For Exams", path: "/student_dashboard" },
-                { name: "Check N. Exam Results", path: "/student_dashboard" },
-                { name: "Check N. Exam Results", path: "/student_dashboard" },
-              ].map((service, index) => (
-                <Link
-                  to={service.path}
-                  index={index}
-                  className="group w-full h-full cursor-pointer text-text-color/90 ring-1 ring-card-bg-weak hover:bg-card-bg-weak rounded-2xl py-4 pl-4 pr-5 shadow-sm flex flex-col justify-between items-start gap-0"
-                >
-                  <h1 className="capitalize text-base leading-6 font-medium group-hover:text-main-color ">
-                    {service.name}
-                  </h1>
-                  <div className="w-full flex items-start justify-start text-main-color mt-4 text-xl">
-                    <LuArrowUpRight />
-                  </div>
-                </Link>
-              ))}
-            </div>
+          <div className="w-full h-fit grid grid-cols-5 max-lg:grid-cols-4 max-md:grid-cols-3 max-sm:grid-cols-2 gap-4 mt-3 py-1">
+            {[
+              { name: "Check Results", path: "/student_dashboard" },
+              { name: "Register For Exams", path: "/student_dashboard" },
+              { name: "Check N. Exam Results", path: "/student_dashboard" },
+              { name: "Check N. Exam Results", path: "/student_dashboard" },
+            ].map((service, index) => (
+              <Link
+                to={service.path}
+                index={index}
+                className="group w-full h-full cursor-pointer text-text-color/90 ring-1 ring-card-bg-weak hover:bg-card-bg-weak rounded-2xl py-4 pl-4 pr-5 shadow-sm flex flex-col justify-between items-start gap-0"
+              >
+                <h1 className="capitalize text-base leading-6 font-medium group-hover:text-main-color ">
+                  {service.name}
+                </h1>
+                <div className="w-full flex items-start justify-start text-main-color mt-4 text-xl">
+                  <LuArrowUpRight />
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>

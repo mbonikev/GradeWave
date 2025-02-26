44497418fdddd05cf3ee19c8a39c869442b03587
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 08:52:44 +0200

    updated src/components/LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -5,7 +5,7 @@ function LoadingScreen({ animateFetching }) {
   return (
     <div
       className={`w-full h-svh fixed top-0 left-0 z-50 bg-white/90 flex items-center justify-center ${
-        animateFetching ? "opacity-0" : ""
+        animateFetching ? "opacity-100" : "opacity-0"
       }`}
     >
       <video

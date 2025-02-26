767cad33103d6d70860faaf9144c7227e213b195
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 21:37:57 +0200

    updated src/pages/student/StudentRegisterForExams.jsx

--- src/pages/student/StudentRegisterForExams.jsx
+++ src/pages/student/StudentRegisterForExams.jsx
@@ -44,7 +44,39 @@ function StudentRegisterForExams() {
           <h1 className="text-sm font-normal my-4 text-text-color-weak">
             # Open Exams
           </h1>
-          <div className="w-full grid grid-cols-3 mt-2">
+          <div className="w-full grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 mt-2">
+            {/* S3 */}
+            <div className="w-full rounded-2xl ring-1 ring-card-bg-weak bg-card-bg-weak/70 shadow-sm p-3 flex items-center">
+              <div className="w-full flex flex-col gap-1 text-sm ">
+                <h1 className="text-base font-bold text-text-color/80">
+                  ORDINARY LEVEL NATIONAL EXAMINATION, 2025-2026
+                </h1>
+                <h1 className="text-text-color/50">
+                  <strong className="font-medium">Registration Status:</strong>{" "}
+                  <span className="text-green-500 font-bold">In Progress</span>
+                </h1>
+
+                <Link
+                  to="/student_register_for_exams"
+                  className="bg-main-color hover:brightness-125 text-white select-none text-sm font-medium px-3 py-2 w-full flex items-center justify-center gap-2 mt-5 rounded-xl"
+                >
+                  Register
+                </Link>
+                {/* <h1 className="text-text-color-weak">
+                <strong className="font-medium">Registration Due Date:</strong> <i> </i> 
+              </h1> */}
+                {/* <h1 className="text-text-color-weak">
+                <strong className="font-medium">Exam Start Date:</strong> <i>July 25, 2025</i> 
+              </h1> */}
+                {/* <h1 className="text-text-color-weak">
+                <strong className="font-medium">Exam End Date:</strong> <i>July 25, 2025</i> 
+              </h1> */}
+                {/* <h1 className="text-text-color-weak">
+                <strong className="font-medium">Exam Center:</strong> <i>Groupe Scolaire Ruyenzi</i> 
+              </h1> */}
+              </div>
+            </div>
+            {/* S6 */}
             <div className="w-full rounded-2xl ring-1 ring-card-bg-weak bg-card-bg-weak/70 shadow-sm p-3 flex items-center">
               <div className="w-full flex flex-col gap-1 text-sm ">
                 <h1 className="text-base font-bold text-text-color/80">

2e12dc87a03c1273753bf16a5e8200cab255be05
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 09:26:16 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -8,7 +8,6 @@ function StudentDashboard() {
   const [showSidebar, setShowSidebar] = useState(false);
   const [animateShowSidebar, setAnimateShowSidebar] = useState(false);
   const [fetching, setFetching] = useState(false);
-  const [showContent, setShowContent] = useState(false);
 
   const openSidebar = () => {
     setShowSidebar(true);
@@ -24,51 +23,32 @@ function StudentDashboard() {
   };
 
   useEffect(() => {
-    const handleReload = () => {
-      setFetching(true);
-      setTimeout(() => {
-        setFetching(false);
-      }, 2000);
-    };
-
-    window.addEventListener("load", handleReload);
-
-    return () => {
-      window.removeEventListener("load", handleReload);
-    };
+    setFetching(true);
   }, []);
 
   return (
-    <>
-      {fetching ? (
-        <LoadingScreen />
-      ) : (
-        <div className="w-full min-h-svh overflow-y-auto flex-1 flex">
-          {/* loading */}
+    <div className="w-full min-h-svh overflow-y-auto flex-1 flex">
+      {/* loading */}
+      {fetching && <LoadingScreen />}
 
-          <StudentSidebar
-            closeSidebar={closeSidebar}
-            showSidebar={showSidebar}
-          />
+      <StudentSidebar closeSidebar={closeSidebar} showSidebar={showSidebar} />
+      <div
+        className={`${
+          showSidebar && ""
+        } max-lg:transition-all max-lg:duration-200 max-lg:ease-in-out w-fit max-lg:w-full flex-1 h-full min-h-svh flex flex-col z-10 bg-body-bg relative`}
+      >
+        {showSidebar && (
           <div
-            className={`${
-              showSidebar && ""
-            } max-lg:transition-all max-lg:duration-200 max-lg:ease-in-out w-fit max-lg:w-full flex-1 h-full min-h-svh flex flex-col z-10 bg-body-bg relative`}
-          >
-            {showSidebar && (
-              <div
-                onClick={closeSidebar}
-                className={`w-full h-full absolute top-0 left-0 bg-black/60 z-30 transition-all duration-300 ease-in-out ${
-                  animateShowSidebar ? "opacity-100" : "opacity-0"
-                }`}
-              ></div>
-            )}
-            <StudentNavbar openSidebar={openSidebar} />
-            <div className="w-full flex-1 overflow-y-auto py-1 flex flex-col gap-0 pt-5 relative"></div>
-          </div>
-        </div>
-      )}
-    </>
+            onClick={closeSidebar}
+            className={`w-full h-full absolute top-0 left-0 bg-black/60 z-30 transition-all duration-300 ease-in-out ${
+              animateShowSidebar ? "opacity-100" : "opacity-0"
+            }`}
+          ></div>
+        )}
+        <StudentNavbar openSidebar={openSidebar} />
+        <div className="w-full flex-1 overflow-y-auto py-1 flex flex-col gap-0 pt-5 relative"></div>
+      </div>
+    </div>
   );
 }
 

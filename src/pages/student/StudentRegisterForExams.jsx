1230bcfe9ea818f238e27bd1fd2f2f64fe557d68
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 21:21:26 +0200

    updated src/pages/student/StudentRegisterForExams.jsx

--- src/pages/student/StudentRegisterForExams.jsx
+++ src/pages/student/StudentRegisterForExams.jsx
@@ -44,13 +44,13 @@ function StudentRegisterForExams() {
           <h1 className="text-sm font-normal my-4 text-text-color-weak"># Open Exams</h1>
           <div className="w-full max-w-[340px] max-md:max-w-full mt-3 rounded-2xl ring-1 ring-card-bg-weak shadow-sm p-3 flex items-center">
             <div className="w-full flex flex-col gap-1 text-sm ">
-              <h1 className="text-base font-medium text-text-color/80">National Examination 2025</h1>
+              <h1 className="text-xl font-medium text-text-color/80">National Examination 2025</h1>
               <h1 className="text-text-color/80">
                 <strong className="font-medium">Registration Status:</strong> <span className="text-green-500 font-bold">In Progress</span>
               </h1>
 
-              <Link to="/student_register_for_exams" className="bg-main-color hover:bg-main-color-dark text-white select-none text-sm font-medium px-3 py-1.5 w-full flex items-center justify-center gap-2 mt-5 rounded-xl">
-                Continue
+              <Link to="/student_register_for_exams" className="bg-main-color hover:bg-main-color-dark text-white select-none text-sm font-medium px-3 py-2 w-full flex items-center justify-center gap-2 mt-5 rounded-xl">
+                Register
               </Link>
               {/* <h1 className="text-text-color-weak">
                 <strong className="font-medium">Registration Due Date:</strong> <i> </i> 

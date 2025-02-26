2114f1d7e400e76ffd928d37a01091f3222817ca
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 10:01:47 +0200

    updated src/assets/util/ProtectedRouteStudent.jsx

--- src/assets/util/ProtectedRouteStudent.jsx
+++ src/assets/util/ProtectedRouteStudent.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 function ProtectedRouteStudent({ children }) {
   const loggedIn = sessionStorage.getItem("loggedInStudent");
+  const navigate = useNavigate() 
 
   if (loggedIn !== true) {
     return navigate("/");

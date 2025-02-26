a55fa086035f3dbf1d78875c95b998d85734ae42
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 09:52:17 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { HashRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import StudentDashboard from "./pages/student/StudentDashboard";
@@ -12,6 +12,7 @@ import StudentNotifications from "./pages/student/StudentNotifications";
 import StudentProfile from "./pages/student/StudentProfile";
 import StudentSettings from "./pages/student/StudentSettings";
 import LoadingScreen from "./components/LoadingScreen";
+import ProtectedRoute from "./ProtectedRoute"; // Import your ProtectedRoute component
 
 function App() {
   const [fetching, setFetching] = useState(true);
@@ -26,6 +27,22 @@ function App() {
       setFetching(false);
     }, 2000);
   }, []);
+
+  // List of routes to protect
+  const protectedRoutes = [
+    { path: "/student_dashboard", element: <StudentDashboard /> },
+    { path: "/student_schools", element: <StudentSchools /> },
+    {
+      path: "/student_register_for_exams",
+      element: <StudentRegisterForExams />,
+    },
+    { path: "/student_check_results", element: <StudentCheckResults /> },
+    { path: "/student_notifications", element: <StudentNotifications /> },
+    { path: "/student_profile", element: <StudentProfile /> },
+    { path: "/student_settings", element: <StudentSettings /> },
+    { path: "/school_dashboard", element: <SchoolDashboard /> },
+  ];
+
   return (
     <div>
       {/* loading */}
@@ -34,28 +51,17 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
+
+          {/* Protect all routes except '/' */}
+          {protectedRoutes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={<ProtectedRoute>{route.element}</ProtectedRoute>}
+            />
+          ))}
+
           <Route path="*" element={<NotFound />} />
-
-          {/* Student */}
-          <Route path="/student_dashboard" element={<StudentDashboard />} />
-          <Route path="/student_schools" element={<StudentSchools />} />
-          <Route
-            path="/student_register_for_exams"
-            element={<StudentRegisterForExams />}
-          />
-          <Route
-            path="/student_check_results"
-            element={<StudentCheckResults />}
-          />
-          <Route
-            path="/student_notifications"
-            element={<StudentNotifications />}
-          />
-          <Route path="/student_profile" element={<StudentProfile />} />
-          <Route path="/student_settings" element={<StudentSettings />} />
-
-          {/* School */}
-          <Route path="/school_dashboard" element={<SchoolDashboard />} />
         </Routes>
       </Router>
     </div>

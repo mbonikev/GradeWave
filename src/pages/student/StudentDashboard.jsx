31fee901ff7505500a80e580d4e49a7b58383e64
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 09:22:46 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -38,28 +38,36 @@ function StudentDashboard() {
   }, []);
 
   return (
-    <div className="w-full min-h-svh overflow-y-auto flex-1 flex">
-      {/* loading */}
-      {fetching && <LoadingScreen />}
+    <>
+      {fetching ? (
+        <LoadingScreen />
+      ) : (
+        <div className="w-full min-h-svh overflow-y-auto flex-1 flex">
+          {/* loading */}
 
-      <StudentSidebar closeSidebar={closeSidebar} showSidebar={showSidebar} />
-      <div
-        className={`${
-          showSidebar && ""
-        } max-lg:transition-all max-lg:duration-200 max-lg:ease-in-out w-fit max-lg:w-full flex-1 h-full min-h-svh flex flex-col z-10 bg-body-bg relative`}
-      >
-        {showSidebar && (
+          <StudentSidebar
+            closeSidebar={closeSidebar}
+            showSidebar={showSidebar}
+          />
           <div
-            onClick={closeSidebar}
-            className={`w-full h-full absolute top-0 left-0 bg-black/60 z-30 transition-all duration-300 ease-in-out ${
-              animateShowSidebar ? "opacity-100" : "opacity-0"
-            }`}
-          ></div>
-        )}
-        <StudentNavbar openSidebar={openSidebar} />
-        <div className="w-full flex-1 overflow-y-auto py-1 flex flex-col gap-0 pt-5 relative"></div>
-      </div>
-    </div>
+            className={`${
+              showSidebar && ""
+            } max-lg:transition-all max-lg:duration-200 max-lg:ease-in-out w-fit max-lg:w-full flex-1 h-full min-h-svh flex flex-col z-10 bg-body-bg relative`}
+          >
+            {showSidebar && (
+              <div
+                onClick={closeSidebar}
+                className={`w-full h-full absolute top-0 left-0 bg-black/60 z-30 transition-all duration-300 ease-in-out ${
+                  animateShowSidebar ? "opacity-100" : "opacity-0"
+                }`}
+              ></div>
+            )}
+            <StudentNavbar openSidebar={openSidebar} />
+            <div className="w-full flex-1 overflow-y-auto py-1 flex flex-col gap-0 pt-5 relative"></div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 

412528ace3ed353834849fac09c76cc31f6c24b4
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 07:18:41 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IntroGif } from "../../assets";
 import StudentSidebar from "../../components/student/StudentSidebar";
+import StudentNavbar from "../../components/student/StudentNavbar";
 
 function StudentDashboard() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [animateShowSidebar, setAnimateShowSidebar] = useState(false);
+  const openSidebar = () => {
+    setShowSidebar(true);
+    setTimeout(() => {
+      setAnimateShowSidebar(true);
+    }, 50);
+  };
+  const closeSidebar = () => {
+    setAnimateShowSidebar(false);
+    setTimeout(() => {
+      setShowSidebar(false);
+    }, 100);
+  };
   return (
     <div className="w-full min-h-svh overflow-y-auto flex-1">
-      <StudentSidebar />
+      <StudentSidebar closeSidebar={closeSidebar} />
+      <div
+        className={`${
+          showSidebar && "max-lg:ml-[255px] max-sm:ml-[100%]"
+        } max-lg:transition-all max-lg:duration-200 max-lg:ease-in-out w-fit max-lg:w-full flex-1 h-full white flex flex-col z-20 bg-white relative`}
+      >
+        {showSidebar && (
+          <div
+            onClick={closeSidebar}
+            className={`w-full h-full absolute top-0 left-0 bg-black/10 z-30 transition-all duration-300 ease-in-out ${
+              animateShowSidebar ? "opacity-100" : "opacity-0"
+            }`}
+          ></div>
+        )}
+        <StudentNavbar openSidebar={openSidebar} />
+      </div>
     </div>
   );
 }

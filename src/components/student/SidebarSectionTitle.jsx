86e0b4e6f123260119123e762acc4c60c8bcd26e
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 07:18:41 +0200

    updated src/components/student/SidebarSectionTitle.jsx

---
+++ src/components/student/SidebarSectionTitle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function SidebarSectionTitle({title}) {
+  return (
+    <p className="py-2 text-xs uppercase font-semibold text-lightBlackText/40">{title}</p>
+
+  )
+}
+
+export default SidebarSectionTitle

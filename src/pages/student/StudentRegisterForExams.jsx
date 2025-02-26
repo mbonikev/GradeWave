c6aa4a069e6524db658faa0a30d9c508aa70f12a
mbonikev <mbonimpayekevin@gmail.com>
2025-02-26 21:24:26 +0200

    updated src/pages/student/StudentRegisterForExams.jsx

--- src/pages/student/StudentRegisterForExams.jsx
+++ src/pages/student/StudentRegisterForExams.jsx
@@ -41,15 +41,23 @@ function StudentRegisterForExams() {
           <h1 className="text-[28px] font-bold tracking-tight mt-3 text-text-color/80 w-full border-b border-card-bg-weak pb-2">
             Register For Exams
           </h1>
-          <h1 className="text-sm font-normal my-4 text-text-color-weak"># Open Exams</h1>
+          <h1 className="text-sm font-normal my-4 text-text-color-weak">
+            # Open Exams
+          </h1>
           <div className="w-full max-w-[340px] max-md:max-w-full mt-3 rounded-2xl ring-1 ring-card-bg-weak bg-card-bg-weak/70 shadow-sm p-3 flex items-center">
             <div className="w-full flex flex-col gap-1 text-sm ">
-              <h1 className="text-xl font-bold text-text-color/80">National Examination 2025</h1>
+              <h1 className="text-xl font-bold text-text-color/80">
+                National Examination 2025
+              </h1>
               <h1 className="text-text-color/80">
-                <strong className="font-medium">Registration Status:</strong> <span className="text-green-500 font-bold">In Progress</span>
+                <strong className="font-medium">Registration Status:</strong>{" "}
+                <span className="text-green-500 font-bold">In Progress</span>
               </h1>
 
-              <Link to="/student_register_for_exams" className="bg-main-color hover:brightness-125 text-white select-none text-sm font-medium px-3 py-2 w-full flex items-center justify-center gap-2 mt-5 rounded-xl">
+              <Link
+                to="/student_register_for_exams"
+                className="bg-main-color hover:brightness-125 text-white select-none text-sm font-medium px-3 py-2 w-full flex items-center justify-center gap-2 mt-5 rounded-xl"
+              >
                 Register
               </Link>
               {/* <h1 className="text-text-color-weak">
@@ -66,8 +74,29 @@ function StudentRegisterForExams() {
               </h1> */}
             </div>
           </div>
-          <h1 className="text-sm font-normal my-4 text-text-color-weak mt-10"># Past Exams</h1>
-          <div className=""></div>
+          <h1 className="text-sm font-normal my-4 text-text-color-weak mt-10">
+            # Past Exams
+          </h1>
+          <div className="w-full grid grid-cols-3">
+            <div className="w-full max-w-[340px] max-md:max-w-full mt-3 rounded-2xl ring-1 ring-card-bg-weak bg-card-bg-weak/70 shadow-sm p-3 flex items-center">
+              <div className="w-full flex flex-col gap-1 text-sm ">
+                <h1 className="text-xl font-bold text-text-color/80">
+                  National Examination 2025
+                </h1>
+                <h1 className="text-text-color/80">
+                  <strong className="font-medium">Registration Status:</strong>{" "}
+                  <span className="text-green-500 font-bold">In Progress</span>
+                </h1>
+
+                <Link
+                  to="/student_register_for_exams"
+                  className="bg-main-color hover:brightness-125 text-white select-none text-sm font-medium px-3 py-2 w-full flex items-center justify-center gap-2 mt-5 rounded-xl"
+                >
+                  Register
+                </Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

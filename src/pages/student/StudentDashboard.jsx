cb6805f3ed916637d093c37e0a3047e965dc7087
mbonikev <mbonimpayekevin@gmail.com>
2025-03-07 07:47:14 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -76,7 +76,7 @@ function StudentDashboard() {
           </div>
           <div className="w-full h-fit grid grid-cols-2 max-md:grid-cols-1 gap-4 mt-3 py-1">
             <div className="w-full h-fit flex flex-col gap-4 mt-3 py-1">
-              <h1 className="text-base font-medium tracking-tight mt-3 text-text-color/80">
+              <h1 className="text-base font-medium tracking-tight mt-3 text-text-color/80 flex items-center justify-between">
                 Announcements
               </h1>
               {[

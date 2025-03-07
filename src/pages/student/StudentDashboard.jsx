27956b2d4138aaea1008b457f95b9cf72ae7cf92
mbonikev <mbonimpayekevin@gmail.com>
2025-03-07 07:36:14 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -70,6 +70,7 @@ function StudentDashboard() {
             ))}
           </div>
           <div className="w-full h-fit grid grid-cols-2 max-md:grid-cols-1 gap-4 mt-3 py-1">
+          <div className="w-full h-fit flex flex-col gap-4 mt-3 py-1">
             <h1 className="text-xl font-medium tracking-tight mt-3 text-text-color/80">
               Announcements
             </h1>
@@ -82,9 +83,12 @@ function StudentDashboard() {
                   "Reminder: students 4 days left to register for exams, the applications are closing on 28th February, 2025. If you have any issue, reach out via email.",
               },
             ].map((annoucement, index) => (
-              <div key={index} className="w-full h-fit p-4 bg-card-bg-weak rounded-xl"></div>
+              <div key={index} className="w-full h-fit p-4 bg-card-bg-weak rounded-xl">
+                <div className="w-full flex items-center"></div>
+              </div>
             ))}
           </div>
+        </div>
         </div>
       </div>
     </div>

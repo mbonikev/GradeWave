3b45d66890ed9468e3f7d98f6d0e78c674edd05b
mbonikev <mbonimpayekevin@gmail.com>
2025-03-08 20:33:11 +0200

    updated src/components/ConfirmLogout.jsx

--- src/components/ConfirmLogout.jsx
+++ src/components/ConfirmLogout.jsx
@@ -3,7 +3,7 @@ import Reveal, { Fade } from "react-awesome-reveal";
 import { keyframes } from "@emotion/react";
 import { TiWarning } from "react-icons/ti";
 import Loading from "./Loading";
-function ConfirmLogout() {
+function ConfirmLogout({logoutWarn, setLogoutWarn}) {
   const [loggingOut, setLoggingOut] = useState(false);
   const handleLoginStudent = () => {
     sessionStorage.removeItem("loggedInStudent");

5eb9f2c7110a40d2be7610a978bdd660bb2fe040
mbonikev <mbonimpayekevin@gmail.com>
2025-03-08 21:48:43 +0200

    updated src/assets/index.jsx

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -4,3 +4,4 @@ export { default as LogoBlue } from "./logo_blue.svg";
 export { default as ImageStudents } from "./student.jpg";
 export { default as IntroGif } from "./intro.mp4";
 export { default as Pfp } from "./pfp.png";
+export { default as School } from "./school.jpg";

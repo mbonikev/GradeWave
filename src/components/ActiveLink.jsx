b26ea58a3a7239ba41ff99bdcc25dd9509febd8b
mbonikev <mbonimpayekevin@gmail.com>
2025-03-08 21:05:12 +0200

    updated src/components/ActiveLink.jsx

---
+++ src/components/ActiveLink.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ActiveLink() {
+  return (
+    <div className="absolute top-0 bottom-0 left-[-10px] my-auto h-0 w-[5px] rounded-[15px] bg-red-400 animate-sidebarIndicator "></div>
+  );
+}
+
+export default ActiveSidebarLink;

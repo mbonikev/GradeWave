526b5e6772199dab95ca3997707b1ed40bca345b
mbonikev <mbonimpayekevin@gmail.com>
2025-03-08 20:29:41 +0200

    updated src/pages/student/StudentDashboard.jsx

--- src/pages/student/StudentDashboard.jsx
+++ src/pages/student/StudentDashboard.jsx
@@ -34,16 +34,7 @@ function StudentDashboard() {
   const [gradesData, setGradesData] = useState([]);
   const [fetching, setFetching] = useState(false);
   const [marks, setMarks] = useState(87);
-  const [loggingOut, setLoggingOut] = useState(false);
   const [logoutWarn, setLogoutWarn] = useState(false);
-  const handleLoginStudent = () => {
-    sessionStorage.removeItem("loggedInStudent");
-    setLoggingOut(true);
-    setTimeout(() => {
-      window.location.reload();
-    }, 300);
-  };
-
   const openSidebar = () => {
     setShowSidebar(true);
     setTimeout(() => {
@@ -214,48 +205,7 @@ function StudentDashboard() {
     <div className="w-full h-fit flex-1 flex relative">
       {/* confirm */}
       {logoutWarn && (
-        <div
-          className={`fixed top-0 left-0 w-full h-full flex items-center justify-center z-50`}
-        >
-          <Reveal
-            duration={200}
-            triggerOnce
-            keyframes={customAnimation}
-            className="w-full h-full"
-          >
-            <div className="w-full h-full relative flex items-center justify-center">
-              <div
-                onClick={() => setLogoutWarn(false)}
-                className=" w-full h-full absolute top-0 left-0 bg-black/15 z-10"
-              ></div>
-              <div
-                className={`w-[280px] h-fit rounded-2xl bg-body-bg  shadow-lg p-5 z-20 ${
-                  logoutWarn ? "scale-100 transition-all " : "scale-75"
-                } `}
-              >
-                <TiWarning className="text-[40px] mx-auto text-amber-500" />
-                <h1 className="mb-5 text-lg text-center max-w-[200px] mx-auto">
-                  Are you sure you want to log out?
-                </h1>
-                <div className="w-full flex items-center flex-col gap-2">
-                  <button
-                    onClick={handleLoginStudent}
-                    className="bg-main-color rounded-2xl border-none transition active:scale-95 text-white w-full flex items-center justify-center h-[42px]"
-                  >
-                    {!loggingOut ? "Logout" : <LoadingWhite size="small" />}
-                  </button>
-                  <button
-                    onClick={() => setLogoutWarn(false)}
-                    className="border border-card-bg rounded-2xl transition active:scale-95 text-text-color w-full flex items-center justify-center h-[40px]"
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </div>
-          </Reveal>
-          <div></div>
-        </div>
+        
       )}
       <StudentSidebar closeSidebar={closeSidebar} showSidebar={showSidebar} />
       <div

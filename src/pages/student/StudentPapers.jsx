ac37ee599e18dba01cb3401f2ffa849439459f7c
mbonikev <mbonimpayekevin@gmail.com>
2025-03-08 20:37:11 +0200

    updated src/pages/student/StudentPapers.jsx

--- src/pages/student/StudentPapers.jsx
+++ src/pages/student/StudentPapers.jsx
@@ -67,7 +67,7 @@ function StudentPapers() {
             }`}
           ></div>
         )}
-        <StudentNavbar openSidebar={openSidebar} />
+        <StudentNavbar openSidebar={openSidebar} setLogoutWarn={setLogoutWarn} />
         <div className="w-full flex-1 max-w-[1120px] px-5 mx-auto pb-10">
           <h1 className="text-[28px] font-bold text-left tracking-tight mt-3 text-text-color/80 w-full border-b border-card-bg-weak pb-2">
             Past Papers

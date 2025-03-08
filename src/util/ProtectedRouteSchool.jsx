820c48b3c743aa89a8c86c57522f44383c2fb8f1
mbonikev <mbonimpayekevin@gmail.com>
2025-03-08 20:49:11 +0200

    updated src/util/ProtectedRouteSchool.jsx

---
+++ src/util/ProtectedRouteSchool.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRouteSchool({ children }) {
+  const loggedIn = sessionStorage.getItem("loggedInStudent");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loggedIn !== 'true') {
+      navigate("/");
+    }
+  }, [loggedIn, navigate]);
+
+  if (loggedIn !== 'true') {
+    return null;
+  }
+
+  return <>{children}</>;
+}
+
+export default ProtectedRouteSchool;

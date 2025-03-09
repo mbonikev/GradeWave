451f148a47fd3cf2c8200dae53b2a30db40beb2b
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 13:56:13 +0200

    updated src/pages/school/SchoolSettings.jsx

--- src/pages/school/SchoolSettings.jsx
+++ src/pages/school/SchoolSettings.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { IntroGif } from "../../assets";
-import StudentSidebar from "../../components/student/StudentSidebar";
-import StudentNavbar from "../../components/student/StudentNavbar";
 import BasicInfo from "./BasicInfo";
 import Security from "./Security";
 import ConfirmLogout from "../../components/ConfirmLogout";
+import SchoolSidebar from "../../components/school/SchoolSidebar";
+import SchoolNavbar from "../../components/school/SchoolNavbar";
 
 function SchoolSettings() {
   const [showSidebar, setShowSidebar] = useState(false);
@@ -34,7 +34,7 @@ function SchoolSettings() {
       {logoutWarn && (
         <ConfirmLogout logoutWarn={logoutWarn} setLogoutWarn={setLogoutWarn} />
       )}
-      <StudentSidebar closeSidebar={closeSidebar} showSidebar={showSidebar} />
+      <SchoolSidebar closeSidebar={closeSidebar} showSidebar={showSidebar} />
       <div
         className={`${
           showSidebar && ""
@@ -48,7 +48,7 @@ function SchoolSettings() {
             }`}
           ></div>
         )}
-        <StudentNavbar openSidebar={openSidebar} setLogoutWarn={setLogoutWarn} />
+        <SchoolNavbar openSidebar={openSidebar} setLogoutWarn={setLogoutWarn} />
         <div className="w-full flex-1 max-w-[1120px] px-5 mx-auto pb-10">
           <h1 className="text-[28px] font-bold text-left tracking-tight mt-3 text-text-color/80 w-full border-b border-card-bg-weak pb-2">
             Settings

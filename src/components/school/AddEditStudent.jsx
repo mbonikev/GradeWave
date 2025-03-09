e032648affee83533f10d2642ed4e26515dfa4b7
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 13:16:11 +0200

    updated src/components/school/AddEditStudent.jsx

--- src/components/school/AddEditStudent.jsx
+++ src/components/school/AddEditStudent.jsx
@@ -193,7 +193,7 @@ function AddEditStudent({ showEditModal, setShowEditModal, level, editMode }) {
                   <input
                     required
                     value={editMode ? residence.district : ""}
-                    onChange={(e) => setresidence(e.target.value)}
+                    onChange={(e) => setResidence(e.target.value)}
                     placeholder="Enter District"
                     className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
                   />

da62f5a60bee0f4b028aab726967d6fd2d2cd183
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 19:54:19 +0200

    updated src/pages/admin/AdminExams.jsx

--- src/pages/admin/AdminExams.jsx
+++ src/pages/admin/AdminExams.jsx
@@ -122,7 +122,7 @@ function AdminExams() {
                 </h1>
                 <h1 className="text-text-color/50">
                   <strong className="font-medium">Registered:</strong>{" "}
-                  <span className="font-medium text-text-color/80">2,239</span>
+                  <span className="font-medium text-text-color/80">{exam.count}</span>
                 </h1>
               </div>
               <Link

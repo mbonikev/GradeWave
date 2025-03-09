7edd1203456be75f1329debb507065a2413f550e
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 12:30:08 +0200

    updated src/components/school/AddEditStudent.jsx

--- src/components/school/AddEditStudent.jsx
+++ src/components/school/AddEditStudent.jsx
@@ -129,8 +129,8 @@ function AddEditStudent({ showEditModal, setShowEditModal }) {
                     required
                     className="border-2 bg-transparent border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
                   >
-                    <option disabled hidden>
-                      Select Relationship
+                    <option disabled hidden selected>
+                      Select Combination
                     </option>
                     <option value="Father">Father</option>
                     <option value="Mother">Mother</option>

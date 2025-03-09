d790e1e8c90215860242e4fdc22614189a6a0363
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 09:22:57 +0200

    updated src/pages/school/SchoolManageStudents.jsx

--- src/pages/school/SchoolManageStudents.jsx
+++ src/pages/school/SchoolManageStudents.jsx
@@ -109,7 +109,7 @@ function SchoolManageStudents() {
                         <tr key={idx} className="border-t">
                           <td className="px-4 py-2 w-[120px]">
                             <Link
-                              to={"/"}
+                              to={`/details?year=${promotion.year}&level=${level.name}`}
                               className="text-main-color-school hover:underline"
                             >
                               {level.name}

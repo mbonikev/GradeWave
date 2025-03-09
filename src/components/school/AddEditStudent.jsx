95e352eea93bc84d607cea43792267ab2a173f04
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 13:42:13 +0200

    updated src/components/school/AddEditStudent.jsx

--- src/components/school/AddEditStudent.jsx
+++ src/components/school/AddEditStudent.jsx
@@ -100,7 +100,7 @@ function AddEditStudent({ showEditModal, setShowEditModal, level, editMode }) {
                     </h1>
                     <select
                       required
-                      value={combination}Select Combination"}
+                      value={combination}
                       onChange={(e) => setCombination(e.target.value)}
                       className="border-2 bg-transparent border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
                     >

3c9029cf12ce2a967fba6f452baeb059d02c612b
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 12:46:39 +0200

    updated src/components/school/AddEditStudent.jsx

--- src/components/school/AddEditStudent.jsx
+++ src/components/school/AddEditStudent.jsx
@@ -76,7 +76,7 @@ function AddEditStudent({
               <h1 className="text-sm font-normal opacity-80 text-text-color-weak/60 italic mb-2">
                 Basic Info <span className="text-red-500">*</span>
               </h1>
-              {editMode ? studentDetails.studentName : ""}
+              
               <div className="w-full grid grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-5">
                 <div className="w-full h-fit">
                   <h1 className="text-base font-normal opacity-80 mb-2">

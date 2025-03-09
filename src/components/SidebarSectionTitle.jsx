14d936860b0e64539265a0b555d85e82e74fedf9
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 19:29:48 +0200

    updated src/components/SidebarSectionTitle.jsx

--- src/components/SidebarSectionTitle.jsx
+++ src/components/SidebarSectionTitle.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SidebarSectionTitle({ title }) {
   return (
-    <p className="py-t mt-3 text-xs uppercase font-medium text-text-color-weak/80">
+    <p className="py-1 mt-3 text-xs uppercase font-medium text-text-color-weak/80">
       {title}
     </p>
   );

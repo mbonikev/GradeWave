f3092d278a600865281b5c2fa6108618e43bb5ec
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 16:50:34 +0200

    updated src/util/ProtectedAuth.jsx

--- src/util/ProtectedAuth.jsx
+++ src/util/ProtectedAuth.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 function ProtectedAuthStudent({ children }) {
   const loggedIn = sessionStorage.getItem("loggedInStudent");
   const loggedInSchool = sessionStorage.getItem("loggedInSchool");
+  const loggedInAdmin = sessionStorage.getItem("loggedInAdmin");
   const navigate = useNavigate();
   const [redirecting, setRedirecting] = useState(false);
   const [showContent, setShowContent] = useState(false);

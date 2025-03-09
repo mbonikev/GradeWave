eb6ed5c24895ed085ee10acf4057a9a574429deb
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 20:09:20 +0200

    updated src/pages/admin/AdminExams.jsx

--- src/pages/admin/AdminExams.jsx
+++ src/pages/admin/AdminExams.jsx
@@ -48,13 +48,7 @@ function AdminExams() {
             <h1 className="text-[28px] font-bold tracking-tight mt-3 text-text-color/80 w-full">
               Exams 2025-2026
             </h1>
-            <button
-              // onClick={() => setShowEditModal(true)}
-              className="py-2 mt-3 px-4 max-sm:px-2.5 w-fit whitespace-nowrap rounded-xl flex items-center justify-center gap-1 bg-transparent hover:bg-main-color active:scale-[.98] text-text-color hover:text-white text-sm font-medium"
-            >
-              <LuPlus className="text-xl" />
-              <span className="max-sm:hidden">New Promotion</span>
-            </button>
+            
           </div>
           <h1 className="text-sm font-normal my-4 text-text-color-weak">
             # Primary

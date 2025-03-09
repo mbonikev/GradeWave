08dee4bd18d17025b537aef69d626c21f3e9f389
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 12:43:09 +0200

    updated src/components/school/AddEditStudent.jsx

--- src/components/school/AddEditStudent.jsx
+++ src/components/school/AddEditStudent.jsx
@@ -74,6 +74,7 @@ function AddEditStudent({ showEditModal, setShowEditModal, level, year, studentD
                   <input
                     required
                     type="number"
+                    value={editMode ? '' : ''}
                     placeholder="Age"
                     className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
                   />

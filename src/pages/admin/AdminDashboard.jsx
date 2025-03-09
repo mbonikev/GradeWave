65edb6a7e808a0bea02247bba72cc0cbeea66aa9
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 16:53:04 +0200

    updated src/pages/admin/

---
+++ src/pages/admin/AdminDashboard.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import SchoolNavbar from "../../components/school/SchoolNavbar";
+import SchoolSidebar from "../../components/school/SchoolSidebar";
+import ConfirmLogout from "../../components/ConfirmLogout";
+
+function NewPage() {
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [animateShowSidebar, setAnimateShowSidebar] = useState(false);
+  const [logoutWarn, setLogoutWarn] = useState(false);
+  const openSidebar = () => {
+    setShowSidebar(true);
+    setTimeout(() => {
+      setAnimateShowSidebar(true);
+    }, 50);
+  };
+  const closeSidebar = () => {
+    setAnimateShowSidebar(false);
+    setTimeout(() => {
+      setShowSidebar(false);
+    }, 100);
+  };
+
+  return (
+    <div className="w-full h-fit flex-1 flex relative">
+      {/* confirm */}
+      {logoutWarn && (
+        <ConfirmLogout logoutWarn={logoutWarn} setLogoutWarn={setLogoutWarn} />
+      )}
+      <SchoolSidebar closeSidebar={closeSidebar} showSidebar={showSidebar} />
+      <div
+        className={`${
+          showSidebar && ""
+        } max-lg:transition-all max-lg:duration-200 max-lg:ease-in-out w-fit max-lg:w-full flex-1 h-svh flex flex-col z-10 bg-white overflow-y-auto relative`}
+      >
+        {showSidebar && (
+          <div
+            onClick={closeSidebar}
+            className={`w-full h-full absolute top-0 left-0 bg-black/60 z-30 transition-all duration-300 ease-in-out ${
+              animateShowSidebar ? "opacity-100" : "opacity-0"
+            }`}
+          ></div>
+        )}
+        <SchoolNavbar setLogoutWarn={setLogoutWarn} openSidebar={openSidebar} />
+        {/* content */}
+        <div className="w-full h-fit max-w-[1120px] px-5 mb-10 mx-auto">
+          <h1 className="text-[28px] font-bold tracking-tight mt-3 text-text-color/80">
+            Manage Students
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NewPage;

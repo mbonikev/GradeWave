50df5d7d74ca8a162e5746fdfb62c066d52d87da
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 12:55:39 +0200

    updated src/pages/school/SchoolPromotionDetails.jsx

--- src/pages/school/SchoolPromotionDetails.jsx
+++ src/pages/school/SchoolPromotionDetails.jsx
@@ -212,7 +212,7 @@ function SchoolPromotionDetails() {
                           Edit
                         </button>
                       ) : (
-                        ""
+                        "-"
                       )}
                     </td>
                   </tr>

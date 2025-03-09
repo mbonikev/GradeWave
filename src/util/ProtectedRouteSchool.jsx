80101223a899d7a276523055a9429d7fbabbea9c
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 16:56:55 +0200

    updated src/util/ProtectedRouteSchool.jsx

--- src/util/ProtectedRouteSchool.jsx
+++ src/util/ProtectedRouteSchool.jsx
@@ -1,15 +1,24 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function ProtectedRouteSchool({ children }) {
-  const loggedIn = sessionStorage.getItem("loggedInSchool");
+  const [loggedIn, setLoggedIn] = useState(null); // Track logged-in state
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const loggedInStatus = sessionStorage.getItem("loggedInSchool");
+    setLoggedIn(loggedInStatus); // Update state on first render
+  }, []);
 
   useEffect(() => {
     if (loggedIn !== 'true') {
       navigate("/");
     }
   }, [loggedIn, navigate]);
+
+  if (loggedIn === null) { // Ensure it's only rendering after we check login state
+    return null;
+  }
 
   if (loggedIn !== 'true') {
     return null;

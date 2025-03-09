baf2d0d95123e54835b33f217e9e4edbfceadae5
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 23:03:09 +0200

    updated src/pages/HelpPage.jsx

--- src/pages/HelpPage.jsx
+++ src/pages/HelpPage.jsx
@@ -18,63 +18,164 @@ const HelpPage = () => {
       <div className=" px-6 max-lg:px-2">
         <div className="max-w-4xl mx-auto p-6 max-lg:p-0">
           <div className="space-y-6">
-            <section>
-              <h2 className="text-base font-semibold text-gray-800 mb-3">
-                1. How to Mark Exams
-              </h2>
-              <p className="text-gray-700 text-sm">
-                To mark exams, navigate to the "Admin" section of your
-                dashboard. Select the exam to mark from the list and enter the
-                scores for each student. You can also adjust grading criteria as
-                needed, and finalize marks with a single click.
-              </p>
-            </section>
+            <div className="w-full h-fit min-h-[300px] max-md:min-h-fit mt-4 mb-20 max-md:mb-10">
+              {/* Basic Info */}
+              <h1 className="text-sm font-normal opacity-80 text-text-color-weak/60 italic mb-2">
+                Basic Info <span className="text-red-500">*</span>
+              </h1>
 
-            <section>
-              <h2 className="text-base font-semibold text-gray-800 mb-3">
-                2. Viewing Student Results
-              </h2>
-              <p className="text-gray-700 text-sm">
-                To view student results, go to the "Results" tab. You can filter
-                results by subject, student name, or date. You also have the
-                option to export data to CSV for further analysis.
-              </p>
-            </section>
+              <div className="w-full grid grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-5">
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    Student Names
+                  </h1>
+                  <input
+                    required
+                    onChange={(e) => setStudentName(e.target.value)}
+                    placeholder="Full Names"
+                    value={studentName}
+                    className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  />
+                </div>
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    Student Age
+                  </h1>
+                  <input
+                    required
+                    type="number"
+                    value={studentAge}
+                    onChange={(e) => setStudentAge(e.target.value)}
+                    placeholder="Age"
+                    className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  />
+                </div>
+                {level !== "O-Level" && (
+                  <div className="w-full h-fit">
+                    <h1 className="text-base font-normal opacity-80 mb-2">
+                      Combination
+                    </h1>
+                    <select
+                      required
+                      value={combination}
+                      defaultValue={"Select Combination"}
+                      onChange={(e) => setCombination(e.target.value)}
+                      className="border-2 bg-transparent border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                    >
+                      <option
+                        value={"Select Combination"}
+                        disabled
+                        hidden
+                        selected
+                      >
+                        Select Combination
+                      </option>
+                      {Combinations.map((comb, index) => (
+                        <option key={index} value={comb}>
+                          {comb}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                )}
+              </div>
 
-            <section>
-              <h2 className="text-base font-semibold text-gray-800 mb-3">
-                3. Exam Scheduling
-              </h2>
-              <p className="text-gray-700 text-sm">
-                In the "Exams" section, click on the "Schedule Exam" button.
-                Here, you can define the exam date, duration, and the specific
-                subjects to be included. Be sure to review the schedule before
-                confirming the exam dates.
-              </p>
-            </section>
+              {/* Parent/Guardian Info */}
+              <h1 className="text-sm font-normal opacity-80 text-text-color-weak/60 italic mt-3 mb-2">
+                Parent/Guardian Info <span className="text-red-500">*</span>
+              </h1>
+              <div className="w-full grid grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-5">
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    Guardian Name
+                  </h1>
+                  <input
+                    required
+                    value={guardianName}
+                    onChange={(e) => setGuardianName(e.target.value)}
+                    placeholder="Guardian's Full Name"
+                    className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  />
+                </div>
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    Guardian Contact
+                  </h1>
+                  <input
+                    required
+                    type="tel"
+                    value={guardianContact}
+                    onChange={(e) => setGuardianContact(e.target.value)}
+                    placeholder="Phone Number"
+                    className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  />
+                </div>
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    Guardian Relationship
+                  </h1>
+                  <select
+                    required
+                    value={
+                      editMode ? guardianRelationship : "Select Relationship"
+                    }
+                    onChange={(e) => setGuardianRelationship(e.target.value)}
+                    className="border-2 bg-transparent border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  >
+                    <option disabled hidden selected>
+                      Select Relationship
+                    </option>
+                    <option value="Father">Father</option>
+                    <option value="Mother">Mother</option>
+                    <option value="Sibling">Sibling</option>
+                    <option value="Other">Other</option>
+                  </select>
+                </div>
+              </div>
 
-            <section>
-              <h2 className="text-base font-semibold text-gray-800 mb-3">
-                4. User Management
-              </h2>
-              <p className="text-gray-700 text-sm">
-                Admins can manage users by going to the "User Management"
-                section. Here, you can add new users, assign roles, and
-                deactivate accounts as necessary. Ensure to keep user
-                information up to date.
-              </p>
-            </section>
-
-            <section>
-              <h2 className="text-base font-semibold text-gray-800 mb-3">
-                5. Technical Support
-              </h2>
-              <p className="text-gray-700 text-sm">
-                If you encounter any technical issues, please contact our
-                support team through the "Support" page. We are available 24/7
-                to assist you with any technical challenges.
-              </p>
-            </section>
+              {/* Residence Info */}
+              <h1 className="text-sm font-normal opacity-80 text-text-color-weak/60 italic mt-3 mb-2">
+                Residence Info <span className="text-red-500">*</span>
+              </h1>
+              <div className="w-full grid grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 gap-5">
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    District
+                  </h1>
+                  <input
+                    required
+                    value={district}
+                    onChange={(e) => setDistrict(e.target.value)}
+                    placeholder="Enter District"
+                    className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  />
+                </div>
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    Sector
+                  </h1>
+                  <input
+                    required
+                    value={sector}
+                    onChange={(e) => setSector(e.target.value)}
+                    placeholder="Enter Sector"
+                    className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  />
+                </div>
+                <div className="w-full h-fit">
+                  <h1 className="text-base font-normal opacity-80 mb-2">
+                    Village
+                  </h1>
+                  <input
+                    required
+                    value={village}
+                    onChange={(e) => setVillage(e.target.value)}
+                    placeholder="Enter Village"
+                    className="border-2 border-stone-200 focus:border-main-color-school outline-none font-medium rounded-2xl px-3 py-2 w-full placeholder:text-text-color-weak/60"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>

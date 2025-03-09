35a39d64fe0369148877b6c94710e4334b5aa233
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 22:29:06 +0200

    updated src/content/Schools.js

---
+++ src/content/Schools.js
@@ -0,0 +1,162 @@
+export const Schools = [
+  {
+    name: "Nelson Mandela TVET School",
+    numberOfCandidates: 150,
+    schoolType: "Secondary", // Updated to N/A as this is a TVET school
+    district: "Bugesera",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "Nyamata TVET School",
+    numberOfCandidates: 120,
+    schoolType: "Secondary", // Updated to N/A as this is a TVET school
+    district: "Bugesera",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "COLLEGE DE BUTAMWA",
+    numberOfCandidates: 200,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyarugenge",
+    sector: "Butamwa",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "COLLEGE SAINT ANDRE",
+    numberOfCandidates: 180,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyarugenge",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "GS INSTITUT FILIPPO SMALDONE (IFS)",
+    numberOfCandidates: 160,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyarugenge",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "LYCEE DE KIGALI",
+    numberOfCandidates: 220,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyarugenge",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "LYCEE NOTRE-DAME DE CITEAUX",
+    numberOfCandidates: 190,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyarugenge",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "ES BUMBOGO",
+    numberOfCandidates: 140,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Gasabo",
+    sector: "Bumbogo",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "FAWE GIRLS SCHOOL",
+    numberOfCandidates: 130,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Gasabo",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "KAGARAMA S S",
+    numberOfCandidates: 170,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Kicukiro",
+    sector: "Kagarama",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "ES KANOMBE /EFOTEC",
+    numberOfCandidates: 160,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Kicukiro",
+    sector: "Kanombe",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "COLLEGE DU CHRIST-ROI",
+    numberOfCandidates: 210,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyanza",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "E S DU SAINT-ESPRIT DE NYANZA",
+    numberOfCandidates: 180,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyanza",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "G.S.MATER DEI",
+    numberOfCandidates: 190,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyanza",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "GS GATAGARA",
+    numberOfCandidates: 150,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyanza",
+    sector: "Gatagara",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "EAV MAYAGA",
+    numberOfCandidates: 160,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Nyanza",
+    sector: "Mayaga",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "E.S GIKONKO",
+    numberOfCandidates: 140,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Gisagara",
+    sector: "Gikonko",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "COLLEGE ST BERNARD KANSI",
+    numberOfCandidates: 130,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Gisagara",
+    sector: "Kansi",
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "G.S ST FRANCOIS D",
+    numberOfCandidates: 150,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Gisagara",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+  {
+    name: "G.S ST PHILIPPE NERI DE GISAGARA",
+    numberOfCandidates: 140,
+    schoolType: "Primary & Secondary", // Updated to Primary & Secondary
+    district: "Gisagara",
+    sector: "-", // Updated to N/A
+    village: "-", // Updated to N/A
+  },
+];

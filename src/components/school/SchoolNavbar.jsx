79872f8572a1b143e244e90e0d1d634d0922ccaa
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 13:00:40 +0200

    updated src/components/school/SchoolNavbar.jsx

--- src/components/school/SchoolNavbar.jsx
+++ src/components/school/SchoolNavbar.jsx
@@ -118,7 +118,7 @@ function SchoolNavbar({ title, openSidebar, setLogoutWarn }) {
                     name: "Adminstrator",
                     publishedAt: "24/02/2025",
                     message:
-                      "Reminder: students 4 days left to register for exams, the applications are closing on 28th February. If you have any issue, reach out via email.",
+                      "System: students 4 days left to register for exams, the applications are closing on 28th February. If you have any issue, reach out via email.",
                   },
                   {
                     pfp: LogoWhite,

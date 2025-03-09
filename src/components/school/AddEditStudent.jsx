1b08b02bbe5f01e2d0b38d6d4d4001f14e709060
mbonikev <mbonimpayekevin@gmail.com>
2025-03-09 12:17:37 +0200

    updated src/components/school/AddEditStudent.jsx

--- src/components/school/AddEditStudent.jsx
+++ src/components/school/AddEditStudent.jsx
@@ -77,7 +77,7 @@ function AddEditStudent({ showEditModal, setShowEditModal }) {
                 </div>
               </div>
               {/* Parent/Guardian info */}
-              {/*  */}
+              {/* Residance info */}
             </div>
             {/* save button */}
             <div className="w-full max-w-[300px] max-sm:max-w-full ml-auto max-sm:ml-0 flex items-center max-sm:flex-col gap-2">
